993cedc353edd7f6fa67c52004479f716673afd7
Jayant-stack <jayanthalapati@gmail.com>
2025-03-09 20:30:10 -0400

    Implement Redux actions and reducers for fetching posts: add getPosts action creator, update posts reducer, and connect posts component to Redux store

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,11 +1,12 @@
-export default (posts =[], action) => {
+export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
+            return action.payload;
+        case 'CREATE':
             return posts;
-            case 'CREATE':
-                return posts;
-            
         default:
-            break;
+            return posts;
     }
-}
+};
+
+
